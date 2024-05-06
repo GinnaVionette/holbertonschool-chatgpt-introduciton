96bf65f8f6ab134753eb30b09d06691f62864f01
GinnaVionette <9011@holbertonstudents.com>
2024-05-06 15:05:29 -0400

    change code for checkers

--- debugging/print_arguments.py
+++ debugging/print_arguments.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
 import sys
 
+if len(sys.argv) == 1:
+    print("Usage: python script.py <arguments>")
+    sys.exit(1)
+
 for i in range(1, len(sys.argv)):
     print(sys.argv[i])
 
